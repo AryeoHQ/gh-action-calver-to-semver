d44fc0843c0e5d39b4517ea83edb05c67e88b00c
Jarrod Parkes <parkesfjarrod@gmail.com>
2021-07-02 10:19:31 -0500

    dont allow calver-count to go below zero

--- test.sh
+++ test.sh
@@ -1,5 +1,7 @@
 export version=1.2.3
 
 # echo "minor: $(echo $version | cut -d. -f2)"
+# echo "$(if [[ $(echo $version | cut -d. -f2) ]]; then echo $(echo $version | cut -d. -f2); else echo 0; fi)"
 
-echo "$(if [[ $(echo $version | cut -d. -f2) ]]; then echo $(echo $version | cut -d. -f2); else echo 0; fi)"
+export count=3
+echo "$(if [[ $(expr $count - 1) -lt 0 ]]; then echo 0; else echo $(expr $count - 1); fi)"
