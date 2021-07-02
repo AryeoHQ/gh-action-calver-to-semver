7224275a24446b406a43f57c8b8fa954d2e3ca74
Jarrod Parkes <parkesfjarrod@gmail.com>
2021-07-02 10:53:28 -0500

    fix issues and document tests

--- test.sh
+++ test.sh
@@ -1,7 +1,61 @@
-export version=1.2.3
+#!/bin/bash
 
-# echo "minor: $(echo $version | cut -d. -f2)"
-# echo "$(if [[ $(echo $version | cut -d. -f2) ]]; then echo $(echo $version | cut -d. -f2); else echo 0; fi)"
+# ============================================================
+# INPUTS
+# calver: 2020-07-02
+# calver-count: 1
+# latest-semver: 1.0.3
 
-export count=3
-echo "$(if [[ $(expr $count - 1) -lt 0 ]]; then echo 0; else echo $(expr $count - 1); fi)"
+# EXPECTED OUTPUTS
+# semver: 1.0.4
+# notes: minor change for 2020-07-02
+# ============================================================
+export calver=2020-07-02
+export ccount=1
+export semver=1.0.3
+export ccount=$(expr $ccount - 1)
+export patch=$(if [[ $(echo $semver | cut -d. -f3) ]]; then echo $(echo $semver | cut -d. -f3); else echo 0; fi)
+export minor=$(if [[ $(echo $semver | cut -d. -f2) ]]; then echo $(echo $semver | cut -d. -f2); else echo 0; fi)
+export mcount=$(if [[ -z $minor ]]; then echo 0; else echo $minor; fi)
+echo "::set-output name=semver::1.$ccount.$(if [[ $ccount -eq $mcount ]]; then echo $(expr $patch + 1); else echo 0; fi)"
+echo "::set-output name=notes::$(if [[ $ccount -eq $mcount ]]; then echo Made minor changes for $calver; else echo Made major changes for $calver; fi)"
+
+# ============================================================
+# INPUTS
+# calver: 2020-07-02
+# calver-count: 2
+# latest-semver: 1.0.3
+
+# EXPECTED OUTPUTS
+# semver: 1.1.0
+# notes: major change for 2020-07-02
+# ============================================================
+export calver=2020-07-02
+export ccount=2
+export semver=1.0.3
+export ccount=$(expr $ccount - 1)
+export patch=$(if [[ $(echo $semver | cut -d. -f3) ]]; then echo $(echo $semver | cut -d. -f3); else echo 0; fi)
+export minor=$(if [[ $(echo $semver | cut -d. -f2) ]]; then echo $(echo $semver | cut -d. -f2); else echo 0; fi)
+export mcount=$(if [[ -z $minor ]]; then echo 0; else echo $minor; fi)
+echo "::set-output name=semver::1.$ccount.$(if [[ $ccount -eq $mcount ]]; then echo $(expr $patch + 1); else echo 0; fi)"
+echo "::set-output name=notes::$(if [[ $ccount -eq $mcount ]]; then echo Made minor changes for $calver; else echo Made major changes for $calver; fi)"
+
+# ============================================================
+# INPUTS
+# calver: 2020-07-02
+# calver-count: 1
+# latest-semver: 
+
+# EXPECTED OUTPUTS
+# semver: 1.0.0
+# notes: major change for 2020-07-02
+# ============================================================
+export calver=2020-07-02
+export ccount=1
+export semver=
+export ccount=$(expr $ccount - 1)
+export patch=$(if [[ $(echo $semver | cut -d. -f3) ]]; then echo $(echo $semver | cut -d. -f3); else echo 0; fi)
+export minor=$(if [[ $(echo $semver | cut -d. -f2) ]]; then echo $(echo $semver | cut -d. -f2); else echo 0; fi)
+export mcount=$(if [[ -z $minor ]]; then echo 0; else echo $minor; fi)
+echo "::set-output name=semver::1.$ccount.$(if [[ $ccount -eq $mcount ]]; then echo $(expr $patch + 1); else echo 0; fi)"
+echo "::set-output name=notes::$(if [[ $ccount -eq $mcount ]]; then echo Made minor changes for $calver; else echo Made major changes for $calver; fi)"
